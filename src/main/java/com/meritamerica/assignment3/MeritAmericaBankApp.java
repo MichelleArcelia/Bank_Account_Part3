8eb499504b6b8a3a0454874ac3b9d2bd24d45f90
MichelleArcelia <michellea.neptune@gmail.com>
2020-10-18 16:03:59 -0500

    FINAL CHANGES - MeritAmericaBankApp

--- src/main/java/com/meritamerica/assignment3/MeritAmericaBankApp.java
+++ src/main/java/com/meritamerica/assignment3/MeritAmericaBankApp.java
@@ -1,7 +1,22 @@
+/* Week 4 - Partner Pair Assignment #3
+ * Bank Account Part 3
+ * @date October 15, 2020
+ * PART 3
+ */
+
 package com.meritamerica.assignment3;
+
+import java.io.*;
+import java.text.*;
+import java.util.*;
 
 public class MeritAmericaBankApp {
 	public static void main(String[] args) {
+		
 		MeritBank.readFromFile("src/test/testMeritBank_good.txt");
+		
+		System.out.println(MeritBank.writeToString());
+		
+		MeritBank.writeToFile();
 	}
 }
